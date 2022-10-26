5d88be0832d6aeb52de864f1fcb71066e3e7ec6f
ElliotMJackson <ElliotMJackson@users.noreply.github.com>
2022-10-26 18:49:18 +0000

    Update version to vv0.0.24

--- private/buf/bufcli/bufcli.go
+++ private/buf/bufcli/bufcli.go
@@ -61,7 +61,7 @@ import (
 
 const (
 	// Version is the CLI version of buf.
-	Version = "0.0.23"
+	Version = "v0.0.24"
 
 	inputHTTPSUsernameEnvKey      = "BUF_INPUT_HTTPS_USERNAME"
 	inputHTTPSPasswordEnvKey      = "BUF_INPUT_HTTPS_PASSWORD"
